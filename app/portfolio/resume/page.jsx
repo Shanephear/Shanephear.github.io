b0bcdadd8906ad6c204b8f13c367b28f4d19c460
shanephear jc <shanephearjc@shanephears-Air.lan>
2024-03-20 14:31:55 -0500

    resume updated

--- app/portfolio/resume/page.jsx
+++ app/portfolio/resume/page.jsx
@@ -3,7 +3,7 @@ const page = () => {
   return (
     <section style={{height: 'calc(100vh - 82px)'}} className="pb-48-lt-m standard-container pb-0-important page-animation">
         <p className='text-2xl font-medium mb-4 animate-container'>Resume</p>
-        <embed className="animate-container-2" src="/resume_07_02_24_gmail.pdf" width="100%" height="90%"></embed>
+        <embed className="animate-container-2" src="/resume.pdf" width="100%" height="90%"></embed>
     </section>
   )
 }
